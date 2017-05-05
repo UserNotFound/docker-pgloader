e8d38c2f1379788f8b587aac15943150ad9064e5
Alex Kubacki <alex@aptible.com>
2017-05-05 17:21:05 -0400

    Fixed it?

--- getenv.sh
+++ getenv.sh
@@ -1,2 +1,2 @@
-sed -i 's/__MYSQL_SERVER__/${MYSQL}/' /app/migrate.load
-sed -i 's/__PSQL_SERVER__/${PSQL}/' /app/migrate.load
+sed -i "s^__MYSQL_SERVER__^${MYSQL}^" /app/migrate.load
+sed -i "s^__PSQL_SERVER__^${PSQL}^" /app/migrate.load
